6a566fa127fb2cbbc1af062f6ab5a79fd698f491
sdavidson90 <s.davidson90@hotmail.ca>
2020-06-23 14:15:26 -0700

    insta gallery almost done

--- src/components/instaGallery/instaGallery.tsx
+++ src/components/instaGallery/instaGallery.tsx
@@ -3,20 +3,22 @@ import React from "react";
 const instaGallery: React.FC = () => {
   return (
     <div className="insta">
-      <div className="insta__title">
-        Follow
-        <a href="https://www.instagram.com" className="insta__title-link">
-          @ThreadsByScott
-        </a>{" "}
-        on InstaGram
-      </div>
+      <a
+        href="https://www.instagram.com"
+        className="insta__title-link insta__title"
+      >
+        Follow @ThreadsByScott on InstaGram
+      </a>
       <div className="insta__item">
-        <div className="insta__item-img-one"></div>
-        <div className="insta__item-img-two"></div>
-        <div className="insta__item-img-three"></div>
-        <div className="insta__item-img-four"></div>
-        <div className="insta__item-img-five"></div>
-        <div className="insta__item-img-six"></div>
+        <div className="insta__item-img insta__item-img-one"></div>
+        <div className="insta__item-img insta__item-img-two"></div>
+        <div className="insta__item-img insta__item-img-three"></div>
+        <div className="insta__item-img insta__item-img-four"></div>
+        <div className="insta__item-img insta__item-img-five"></div>
+        <div className="insta__item-img insta__item-img-six"></div>
+        <div className="insta__item-img insta__item-img-seven"></div>
+        <div className="insta__item-img insta__item-img-eight"></div>
+        <div className="insta__item-img insta__item-img-nine"></div>
       </div>
     </div>
   );
