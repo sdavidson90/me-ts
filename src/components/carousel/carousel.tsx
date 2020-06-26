5a52120d32e5d2b3c68726ff587818e9bdec43d0
sdavidson90 <s.davidson90@hotmail.ca>
2020-06-26 16:25:11 -0700

    new items fix

--- src/components/carousel/carousel.tsx
+++ src/components/carousel/carousel.tsx
@@ -6,12 +6,30 @@ const Carousel = () => {
     <div className="clothing">
       <div className="clothing__title">New Arrivals</div>
       <div className="clothing__item">
-        <Link to="/shop" className="clothing__item-img-one"></Link>
-        <Link to="/shop" className="clothing__item-img-two"></Link>
-        <Link to="/shop" className="clothing__item-img-three"></Link>
-        <Link to="/shop" className="clothing__item-img-four"></Link>
-        <Link to="/shop" className="clothing__item-img-five"></Link>
-        <Link to="/shop" className="clothing__item-img-six"></Link>
+        <Link
+          to="/shop"
+          className="clothing__item-img clothing__item-img-one"
+        ></Link>
+        <Link
+          to="/shop"
+          className="clothing__item-img clothing__item-img-two"
+        ></Link>
+        <Link
+          to="/shop"
+          className="clothing__item-img clothing__item-img-three"
+        ></Link>
+        <Link
+          to="/shop"
+          className="clothing__item-img clothing__item-img-four"
+        ></Link>
+        <Link
+          to="/shop"
+          className="clothing__item-img clothing__item-img-five"
+        ></Link>
+        <Link
+          to="/shop"
+          className="clothing__item-img clothing__item-img-six"
+        ></Link>
       </div>
     </div>
   );
